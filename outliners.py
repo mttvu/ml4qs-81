ac14d79eea058516099f6d46f3c9b3b79194bac2
keyezhou <zhoukeye11@gmail.com>
2024-06-09 15:47:57 +0200

    add plot

--- outliners.py
+++ outliners.py
@@ -2,15 +2,11 @@ import numpy as np
 import pandas as pd
 import os
 import multiprocessing
-import matplotlib.pyplot as plt
 import seaborn as sns
 import warnings
 from scipy.special import erfc
 import scipy
-warnings.simplefilter('ignore')
-plt.style.use('seaborn')
-for package in [pd, np, scipy, sns]:
-    print(package.__name__, 'version:', package.__version__)
+import matplotlib.pyplot as plt
 
 def chauvenet(array):
     mean = array.mean()           # Mean of incoming array
@@ -22,8 +18,24 @@ def chauvenet(array):
     return prob < criterion       # Use boolean array outside this function
 
 train_outliers = dict()
-for col in [col for col in train.columns if 'var_' in col]:
-    train_outliers[col] = train[chauvenet(train[col].values)].shape[0]
-train_outliers = pd.Series(train_outliers)
+train = pd.read_csv("./data/my/per_second_data.csv")
+train.fillna(0, inplace=True)
 
-train_outliers.sort_values().plot(figsize=(14, 40), kind='barh').set_xlabel('Number of outliers');
+
+for lable in [lable for lable in train.columns if 'label_'  in lable]:
+    one_lable = train[train[lable]==1.0]
+    for col in [col for col in train.columns if 'label_' not in col and 'time' not in col]:
+        train_outliers[col] = one_lable[chauvenet(one_lable[col].values)].shape[0]
+    train_outliers = pd.Series(train_outliers).sort_values()
+    percentage_outliners = (train_outliers/one_lable.shape[0])*100
+    print(train_outliers)
+    print(one_lable.shape[0])
+    plt.barh(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().values()), align='center')
+    plt.yticks(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().keys()))
+    plt.title("Outliners for lable "+ lable)
+    plt.show()
+    #train_outliers.sort_values().plot(kind='hist', title="My plot").show()
+    #train_outliers.sort_values().plot(figsize=(14, 40), kind='barh').set_xlabel('Number of outliers')
+    print('Total number of outliers in training set: {} ({:.2f}%)'.format(sum(train_outliers.values), np.max(percentage_outliners)))
+    print("this is lable" + lable)
+

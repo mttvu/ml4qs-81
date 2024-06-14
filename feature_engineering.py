b5303266539725a6b1cb3fdc50fe13072fd69895
my vu <myxvux@gmail.com>
2024-06-14 17:26:02 +0200

    added feature engineering

---
+++ feature_engineering.py
@@ -0,0 +1,90 @@
+import sys
+import copy
+import pandas as pd
+import time
+from pathlib import Path
+import argparse
+import numpy as np
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+from util.VisualizeDataset import VisualizeDataset
+from Chapter4.TemporalAbstraction import NumericalAbstraction
+from Chapter4.TemporalAbstraction import CategoricalAbstraction
+from Chapter4.FrequencyAbstraction import FourierTransformation
+
+def extract_date_time_info(df):
+    df['date_time'] = pd.to_datetime(df['date_time'])
+    df['weekday'] = df['date_time'].dt.weekday
+    df['hour'] = df['date_time'].dt.hour
+    df['minute'] = df['date_time'].dt.minute
+    return df
+
+# for example my -> 1, kirsty -> 2, keye -> 3
+def participant_label_encoding(df):
+    label_encoder = LabelEncoder()
+    df['participant_encoded'] = label_encoder.fit_transform(df['participant'])
+    return df
+
+def frequency_domain_transformation(df):
+    sensor_columns = [
+    'acceleration_x', 'acceleration_y', 'acceleration_z',
+    'gyroscope_x', 'gyroscope_y', 'gyroscope_z',
+    'linear_acceleration_x', 'linear_acceleration_y', 'linear_acceleration_z',
+    'magnetic_field_x','magnetic_field_y','magnetic_field_z']
+
+    df = FourierTransformation().abstract_frequency(data_table=df, columns=sensor_columns, window_size=30, sampling_rate=2)
+    
+    return df
+
+def location_transformation(df, window_size):
+    # difference in consecutive rows
+    df['delta_latitude'] = df['location_latitude'].diff()
+    df['delta_longitude'] = df['location_longitude'].diff()
+    df['delta_height'] = df['location_height'].diff()
+    df['delta_velocity'] = df['location_velocity'].diff()
+    df['delta_direction'] = df['location_direction'].diff().abs()
+
+    df['time_diff'] = df['date_time'].diff().dt.total_seconds()  # Time difference in seconds
+    df['rate_delta_velocity'] = df['delta_velocity'] / df['time_diff']
+    df['rate_delta_height'] = df['delta_height'] / df['time_diff']
+
+    df['rolling_cumulative_distance'] = df.apply(
+        lambda row: np.sqrt((df.loc[:row.name, 'delta_latitude']**2 + 
+                            df.loc[:row.name, 'delta_longitude']**2).sum()), axis=1).rolling(window=window_size).sum()
+
+    df['rolling_cumulative_elevation'] = df['delta_height'].rolling(window=window_size).sum().fillna(0)
+    df['rolling_avg_velocity'] = df['location_velocity'].rolling(window=window_size).mean()
+    df['rolling_avg_direction'] = df['location_direction'].rolling(window=window_size).mean()
+
+    return df
+
+def time_domain_transformation(df, window_size):
+    sensor_columns = [
+    'acceleration_x', 'acceleration_y', 'acceleration_z',
+    'gyroscope_x', 'gyroscope_y', 'gyroscope_z',
+    'linear_acceleration_x', 'linear_acceleration_y', 'linear_acceleration_z',
+    'magnetic_field_x','magnetic_field_y','magnetic_field_z', 'pressure']
+    
+    NumericalAbstraction().abstract_numerical(df, sensor_columns, window_size, 'mean')
+    NumericalAbstraction().abstract_numerical(df, sensor_columns, window_size, 'std')
+
+    return df
+
+def standardize(df):
+    exclude_cols = ['index','participant', 'date_time'] + [col for col in df.columns if col.startswith('label_')]
+    cols_to_standardize = df.columns.difference(exclude_cols)
+    scaler = StandardScaler()
+    df[cols_to_standardize] = scaler.fit_transform(df[cols_to_standardize])
+    return df
+
+df = pd.read_csv('data/all_data_per_second.csv')
+df = df.drop(['index','Unnamed: 0','time'], errors='ignore', axis=1)
+df = standardize(df)
+df = extract_date_time_info(df)
+df = participant_label_encoding(df)
+df = frequency_domain_transformation(df)
+df = location_transformation(df, 10)
+df = time_domain_transformation(df, 10)
+
+
+df.to_csv('data/data_fe.csv')
--- outliners.py
+++ outliners.py
@@ -1,55 +1,67 @@
 import numpy as np
 import pandas as pd
-import os
-import multiprocessing
-import seaborn as sns
-import warnings
 from scipy.special import erfc
-import scipy
 import matplotlib.pyplot as plt
 
 def chauvenet(array):
-    mean = array.mean()           # Mean of incoming array
-    stdv = array.std()            # Standard deviation
+    mean = np.nanmean(array)
+    stdv = np.nanstd(array)
+    #mean = array.mean()           # Mean of incoming array
+    #stdv = array.std()            # Standard deviation
+    array[np.isnan(array)] = mean
     N = len(array)                # Lenght of incoming array
     criterion = 1.0/(2*N)         # Chauvenet's criterion
     d = abs(array-mean)/stdv      # Distance of a value to mean in stdv's
     prob = erfc(d)                # Area normal dist.
-    return prob < criterion       # Use boolean array outside this function
+    return  prob < criterion, mean      # Use boolean array outside this function
 
-def clean_outliners(df):
-    df_result = df.iloc[:0, :].copy()
-    for lable in [lable for lable in df.columns if 'label_' in lable]:
-        one_lable = df[df[lable] == 1.0]
-        for col in [col for col in df.columns if 'label_' not in col and 'time' not in col]:
-            tmparray = one_lable[col].values
-            mean = one_lable[col].values.mean()
-            for i in range(one_lable.shape[0]):
-                if tmparray[i]:
-                    one_lable.at[i,col] = mean
-        pd.concat([df_result, one_lable])
-    return df_result
+def clean_outliners(train):
+    for col in [col for col in train.columns if 'label_' not in col and 'time' not in col and 'participant' not in col]:
+        tmp_array, mean = chauvenet(train[col].values)
+        for i in range(tmp_array.size):
+            if tmp_array[i]:
+                print(i, col)
+                train.loc[i, col] = np.nan
+    return train
 
 
 train_outliers = dict()
-train = pd.read_csv("./data/my/per_second_data.csv")
-train.fillna(0, inplace=True)
+train = pd.read_csv("./data/all_data_per_second.csv")
+#train.fillna(0, inplace=True)
 
 
-for lable in [lable for lable in train.columns if 'label_'  in lable]:
-    one_lable = train[train[lable]==1.0]
-    for col in [col for col in train.columns if 'label_' not in col and 'time' not in col]:
-        train_outliers[col] = one_lable[chauvenet(one_lable[col].values)].shape[0]
-    train_outliers = pd.Series(train_outliers).sort_values()
-    percentage_outliners = (train_outliers/one_lable.shape[0])*100
-    plt.barh(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().values()), align='center')
-    plt.yticks(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().keys()))
-    plt.title("Outliners for lable "+ lable)
-    plt.show()
-    #train_outliers.sort_values().plot(kind='hist', title="My plot").show(),
-    #train_outliers.sort_values().plot(figsize=(14, 40), kind='barh').set_xlabel('Number of outliers')
-    print('Total number of outliers in training set: {} ({:.2f}%)'.format(sum(train_outliers.values), np.max(percentage_outliners)))
-    print("this is lable" + lable)
+# for lable in [lable for lable in train.columns if 'label_'  in lable]:
+#     one_lable = train[train[lable]==1.0]
+# for col in [col for col in train.columns if 'label_' not in col and 'time' not in col]:
+#     train_outliers[col] = one_lable[chauvenet(one_lable[col].values)].shape[0]
+# train_outliers = pd.Series(train_outliers).sort_values()
+# percentage_outliners = (train_outliers/one_lable.shape[0])*100
+# plt.barh(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().values()), align='center')
+# plt.yticks(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().keys()))
+# plt.title("Outliners for lable "+ lable)
+# plt.show()
+for col in [col for col in train.columns if 'label_' not in col and 'time' not in col and 'participant' not in col]:
+    tmp_array, mean = chauvenet(train[col].values)
+    train_outliers[col] = train[tmp_array].shape[0]
+train_outliers = pd.Series(train_outliers).sort_values()
+percentage_outliners = (train_outliers/train.shape[0])*100
+plt.barh(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().values()), align='center')
+plt.yticks(range(len(train_outliers.to_dict())), list(train_outliers.to_dict().keys()))
+plt.show()
+print('Total number of outliers in training set: {} ({:.2f}%)'.format(sum(train_outliers.values), np.max(percentage_outliners)))
+# display(train.head())
+for col in [col for col in train.columns if 'label_' not in col and 'time' not in col and 'participant' not in col]:
+    tmp_array, mean = chauvenet(train[col].values)
+    for i in range(tmp_array.size):
+        if tmp_array[i]:
+            train.loc[i, col] = np.NaN
+# for col in [col for col in train.columns if 'label_' not in col and 'time' not in col and 'participant' not in col]:
+#     tmp_array, mean = chauvenet(train[col].values)
+#     for i in range(tmp_array.size):
+#         if tmp_array[i]:
+#             train.loc[i,col] = np.nan
+train.to_csv("./data/eliminated_outliners.csv")
+#print("this is lable" + lable)
 
 # train = clean_outliners(train)
 
